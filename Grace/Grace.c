211ca74d6824db9c17fafb9bf687d61486aa627a
Nikolai <professor3222@gmail.com>
2021-01-09 16:13:14 +0300

    adding fixed Grace.c

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define STR "#include <stdio.h>%c%c#define STR %c%s%c%c%c#define DATA(file) fprintf(file, STR, 10, 10, 34, STR, 34, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10)%c%c#define MAIN int main() {/*comment*/ FILE *file; file = fopen(%cGrace_kid.c%c, %cw%c); DATA(file); fclose(file); return (0); }%c%c%cMAIN%c"
+#define STR "#include <stdio.h>%1$c%1$c#define STR %2$c%3$s%2$c%1$c%1$c#define DATA(file) fprintf(file, STR, 10, 34, STR)%1$c%1$c#define MAIN int main() {/*comment*/ FILE *file; file = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); DATA(file); fclose(file); return (0); }%1$c%1$c%1$cMAIN%1$c"
 
-#define DATA(file) fprintf(file, STR, 10, 10, 34, STR, 34, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10)
+#define DATA(file) fprintf(file, STR, 10, 34, STR)
 
 #define MAIN int main() {/*comment*/ FILE *file; file = fopen("Grace_kid.c", "w"); DATA(file); fclose(file); return (0); }
 
