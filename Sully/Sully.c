bcd58e0d83f6349d2fa1397395c3d1d90c257695
Nikolai <professor3222@gmail.com>
2021-01-09 19:10:58 +0300

    adding Sully

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define STR "#include <stdio.h>%c#include <string.h>%c%c#define STR %c%s%c%c%c#define OUTPUT(file, i) fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 34, 34, 10, 10, i, 10)%c%c#define CYCLE_BODY { char buff[9]; create_filename(i, (char*)&buff); FILE *file = fopen(buff, %cw%c); OUTPUT(file, i); fclose(file); --i; }%c%cvoid create_filename(int num, char* filename) { char buff[1] = {num + '0'}; strcpy(filename, %cSully_%c); strcat(filename, buff); strcat(filename, %c.c%c); }%c%cint main() { int i = %d; while (i >= 0) CYCLE_BODY return 0; }%c"
+#define FILE_NAME "Sully_%d.c"
+#define CMD "gcc -Wall -Wextra -Werror Sully_%d.c -o Sully_%d ; ./Sully_%d"
+#define STR "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$c#define FILE_NAME %2$cSully_%%d.c%2$c%1$c#define CMD %2$cgcc -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d ; ./Sully_%%d%2$c%1$c#define STR %2$c%4$s%2$c%1$c#define START_FILE %2$cSully_5.c%2$c%1$c%1$cint main(void) {%1$cint i = %3$d;%1$cif (i <= 0)%1$creturn 0;%1$cchar cmd[100], file_name[100];%1$c%1$cif (access(START_FILE, F_OK) == 0)%1$ci--;%1$c%1$csprintf(file_name, FILE_NAME, i);%1$csprintf(cmd, CMD, i, i, i);%1$cFILE* file = fopen(file_name, %2$cw%2$c);%1$cif (file == NULL)%1$creturn -1;%1$cfprintf(file, STR, 10, 34, i, STR);%1$cfclose(file);%1$c%1$csystem(cmd);%1$creturn (0);%1$c}%1$c"
+#define START_FILE "Sully_5.c"
 
-#define OUTPUT(file, i) fprintf(file, STR, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 34, 34, 10, 10, 34, 34, 34, 34, 10, 10, i, 10)
+int main(void) {
+int i = 5;
+if (i <= 0)
+return 0;
+char cmd[100], file_name[100];
 
-#define CYCLE_BODY { char buff[9]; create_filename(i, (char*)&buff); FILE *file = fopen(buff, "w"); OUTPUT(file, i); fclose(file); --i; }
+if (access(START_FILE, F_OK) == 0)
+i--;
 
-void create_filename(int num, char* filename) { char buff[1] = {num + '0'}; strcpy(filename, "Sully_"); strcat(filename, buff); strcat(filename, ".c"); }
+sprintf(file_name, FILE_NAME, i);
+sprintf(cmd, CMD, i, i, i);
+FILE* file = fopen(file_name, "w");
+if (file == NULL)
+return -1;
+fprintf(file, STR, 10, 34, i, STR);
+fclose(file);
 
-int main() { int i = 5; while (i >= 0) CYCLE_BODY return 0; }
+system(cmd);
+return (0);
+}
