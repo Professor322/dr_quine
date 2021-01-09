8beefbd5f205f3f20df92f4856ffb70cf9afbeba
Nikolai <professor3222@gmail.com>
2021-01-09 17:26:19 +0300

    adding Colleen

--- Colleen/Colleen.s
+++ Colleen/Colleen.s
@@ -29,4 +29,4 @@ pop %rbp
 ret
 
 .section .data
-fmt: .asciz "#comment outside main%1$c.section .text%1$c.global main%1$cfoo:%1$cpush %rbp%1$cmov %rsp, %rbp%1$clea fmt(%rip), %rdi%1$cmov $10, %rsi%1$cmov $34, %rdx%1$clea fmt(%rip), %rcx%1$cxor %rax, %rax%1$ccall printf@plt%1$cmov %rbp, %rsp%1$cpop %rbp%1$cret%1$c%1$cmain:%1$cpush %rbp%1$cmov %rsp, %rbp%1$c%1$cxor %rax, %rax%1$c#calling foo%1$ccall foo%1$c %1$cmov %rbp, %rsp%1$cpop %rbp%1$cret%1$c%1$c.section .data%1$cfmt: .asciz %2$c%3$s%2$c%1$c"
+fmt: .asciz "#comment outside main%1$c.section .text%1$c.global main%1$cfoo:%1$cpush %rbp%1$cmov %rsp, %rbp%1$c%1$clea fmt(%rip), %rdi%1$cmov $10, %rsi%1$cmov $34, %rdx%1$clea fmt(%rip), %rcx%1$cxor %rax, %rax%1$ccall printf@plt%1$c%1$cmov %rbp, %rsp%1$cpop %rbp%1$cret%1$c%1$cmain:%1$cpush %rbp%1$cmov %rsp, %rbp%1$c%1$cxor %rax, %rax%1$c#calling foo%1$ccall foo%1$c%1$cmov %rbp, %rsp%1$cpop %rbp%1$cret%1$c%1$c.section .data%1$cfmt: .asciz %2$c%3$s%2$c%1$c"
