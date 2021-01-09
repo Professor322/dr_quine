aca2a5a0ba5ad761d9ba600364bb6c8bb037835a
Nikolai <professor3222@gmail.com>
2021-01-09 19:27:10 +0300

    adding file openingn check in Grace,c

--- C/Grace.c
+++ C/Grace.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define STR "#include <stdio.h>%1$c%1$c#define STR %2$c%3$s%2$c%1$c%1$c#define DATA(file) fprintf(file, STR, 10, 34, STR)%1$c%1$c#define MAIN int main() {/*comment*/ FILE *file; file = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); DATA(file); fclose(file); return (0); }%1$c%1$c%1$cMAIN%1$c"
+#define STR "#include <stdio.h>%1$c%1$c#define STR %2$c%3$s%2$c%1$c%1$c#define DATA(file) fprintf(file, STR, 10, 34, STR)%1$c%1$c#define MAIN int main() {/*comment*/ FILE *file; file = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); if (file == NULL) return (-1); DATA(file); fclose(file); return (0); }%1$c%1$c%1$cMAIN%1$c"
 
 #define DATA(file) fprintf(file, STR, 10, 34, STR)
 
-#define MAIN int main() {/*comment*/ FILE *file; file = fopen("Grace_kid.c", "w"); DATA(file); fclose(file); return (0); }
+#define MAIN int main() {/*comment*/ FILE *file; file = fopen("Grace_kid.c", "w"); if (file == NULL) return (-1); DATA(file); fclose(file); return (0); }
 
 
 MAIN
